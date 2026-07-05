6505a78350b248a78a077b8b2b61bb0eac1e6c20
revathirey08 <revathimuthu2004@gmail.com>
2026-07-05 15:57:34 +0530

    initial commit

---
+++ react-app/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import navbar from './navbar';
+import Course from './Course';
+
+function App() {
+  
+
+  return (
+    <>
+    <navbar />
+    <Course></Course>
+    </>
+  );
+}
+    
+
+export default App
---
+++ react-app/src/Navbar.jsx
@@ -0,0 +1,9 @@
+function navbar(){
+    return(
+        <nav>
+        <h2>code io</h2>
+        <button>log in</button>
+        </nav>
+    );
+}
+export default navbar;
